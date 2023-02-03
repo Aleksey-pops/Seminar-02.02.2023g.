59b601d0fae7301ad7fc90bf3d138c24936383b3
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-03 22:26:27 +0300

    The number of digits of numbers including. second option.

--- The number of digits of numbers including./Program.cs
+++ The number of digits of numbers including./Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Сколько цифр в числе (разрядов)
+
+// Console.WriteLine("Введите первое число: " ?? "0");
+// int number = int.Parse(Console.ReadLine());
+
+// int count = 1;
+// int res = Math.Abs(number);
+// while (res > 1)
+// {
+//     res = res / 10;
+//     count ++;    
+// }
+// Console .WriteLine("Количество цифр в числе: "+ count);
+
+// Сколько цифр в числе (разрядов) Вариант 2
+
+Console.WriteLine("Введите первое число: " ?? "0");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+
+for (int res = Math.Abs(number); (res > 1); res = res /10, count++);
+{
+   Console .WriteLine("Количество цифр в числе: "+ count); 
+}
+//  С doble больше чисел считает!!!
