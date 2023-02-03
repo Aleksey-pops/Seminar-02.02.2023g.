43506f96b4bbd4dd2af2b6b443f896e82d572f55
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-03 21:30:16 +0300

    Find the sum of numbers from 1 to A second option.

--- Find the sum of number sfrom 1 to A/Program.cs
+++ Find the sum of number sfrom 1 to A/Program.cs
@@ -1,14 +1,29 @@
 ﻿// Найти сумму чисел от 1 до А
 
-Console.WriteLine("Enter number: ");
+// Console.WriteLine("Enter number: ");
+// int A = int.Parse(Console.ReadLine());
+// int sum = 0;
+
+// for(int i = 1; i <= A; i++){
+
+//     sum = sum + i;
+//     Console.WriteLine("i = " );
+//     Console.WriteLine("Sum" + i + "=" + sum);
+// }
+
+// Console.WriteLine("Sum = "+ sum );
+
+// Найти сумму чисел от 1 до А вторая версия
+ 
+ Console.WriteLine("Введите число : " ?? "0");
 int A = int.Parse(Console.ReadLine());
+int counter =1;
 int sum = 0;
 
-for(int i = 1; i <= A; i++){
-
-    sum = sum + i;
-    Console.WriteLine("i = " );
-    Console.WriteLine("Sum" + i + "=" + sum);
+while (counter <= A)
+{
+    sum = sum + counter; 
+    counter ++;
 }
 
-Console.WriteLine("Sum = "+ sum );
+Console.WriteLine("Сумма чисел от 1 " + "до А равна: " + sum);
