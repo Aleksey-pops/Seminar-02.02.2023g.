a941d8456b99219e11e40f92fb3c8d43e5eb5e8f
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-03 23:02:16 +0300

    How many digits including (digits) the third option.

--- The number of digits of numbers including./Program.cs
+++ The number of digits of numbers including./Program.cs
@@ -1,10 +1,11 @@
-﻿// // Сколько цифр в числе (разрядов)
+﻿using System;
+// // Сколько цифр в числе (разрядов)
 
 // Console.WriteLine("Введите первое число: " ?? "0");
-// int number = int.Parse(Console.ReadLine());
+// int A = int.Parse(Console.ReadLine());
 
 // int count = 1;
-// int res = Math.Abs(number);
+// int res = Math.Abs(A);
 // while (res > 1)
 // {
 //     res = res / 10;
@@ -12,14 +13,36 @@
 // }
 // Console .WriteLine("Количество цифр в числе: "+ count);
 
-// Сколько цифр в числе (разрядов) Вариант 2
+
+
+// // // Сколько цифр в числе (разрядов) Вариант 2
+
+// // Console.WriteLine("Введите первое число: " ?? "0");
+// // int A = int.Parse(Console.ReadLine());
+// // int count = 1;
+
+// // for (int res = Math.Abs(A); (res > 1); res = res /10, count++);
+// // {
+// //    Console .WriteLine("Количество цифр в числе: "+ count); 
+// }
+
+//  С doble больше чисел считает!!!
 
 Console.WriteLine("Введите первое число: " ?? "0");
-int number = int.Parse(Console.ReadLine());
-int count = 1;
+int A = int.Parse(Console.ReadLine());
 
-for (int res = Math.Abs(number); (res > 1); res = res /10, count++);
+int counter = 0;
+if (A == 1)
 {
-   Console .WriteLine("Количество цифр в числе: "+ count); 
+    counter ++;
+    Console.WriteLine($"couter = {counter}");
 }
-//  С doble больше чисел считает!!!
+else
+{
+    while (A!= 0)
+    {
+        counter++;
+        A = A / 10;
+    }
+    Console.WriteLine($"counter = {counter}");
+}
