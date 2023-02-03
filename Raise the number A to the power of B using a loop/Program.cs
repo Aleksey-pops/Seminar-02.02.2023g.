091a79f5bda34043e32a2bb90cd89441712e6934
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-03 22:04:00 +0300

    The number of digits of numbers including.

--- Raise the number A to the power of B using a loop/Program.cs
+++ Raise the number A to the power of B using a loop/Program.cs
@@ -15,25 +15,46 @@
 // Console.WriteLine($"Sum = {sum}");
 // // A^B
 
-// Возведите число А в натуральную степень B используя цикл
+// // Возведите число А в натуральную степень B используя цикл
 
-Console.WriteLine("Enter 1st number: ");
-int A = int.Parse(Console.ReadLine());
+// // Console.WriteLine("Enter 1st number: " ?? "0");
+// // int A = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter 2nd number: ");
-int B = int.Parse(Console.ReadLine());
+// // Console.WriteLine("Enter 2nd number: ");
+// // int B = int.Parse(Console.ReadLine());
 
-int sum = A;
-if(B == 0)
+// // int sum = A;
+// // if(B == 0)
+// // {
+// //     Console.WriteLine("Sum = 1");
+// // }
+// // else
+// // {
+// //     for(int i = 1; i < B; i++)
+// //     {
+
+// //         sum = sum * A;
+// //     }
+// //     Console.WriteLine($"Sum = {sum}");
+// }
+// ??? Почему то при возведении в степень 12 и больше появляется число с минусом???
+
+Console.WriteLine("Введите число : " ?? "0");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите степень числа : ");
+int step = int.Parse(Console.ReadLine());
+
+int res = 1;
+
+if (step >= 0)
 {
-    Console.WriteLine("Sum = 1");
+    for (int i = 0; i < step; i++) {
+    res = number * res;
+    }
+    Console.WriteLine("Результат: " + res);
 }
 else
 {
-    for(int i = 1; i < B; i++)
-    {
-
-        sum = sum * A;
-    }
-    Console.WriteLine($"Sum = {sum}");
+    Console.WriteLine("Требуется положительная степень!");
 }
